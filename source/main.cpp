d1d8fa737765c05f426c1b88ab84c8b5945d7679
a1824273 <a1824273@student.adelaide.edu.au>
2024-07-25 21:28:38 +0930

    about to make a pyramid

--- source/main.cpp
+++ source/main.cpp
@@ -1,57 +1,138 @@
 #include <iostream>
+#include <cmath>
 
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
-void ProcessInput(GLFWwindow* window)
+#include "../includes/stb_image.h"
+
+#include "../includes/EBO.h"
+#include "../includes/VAO.h"
+#include "../includes/VBO.h"
+#include "../includes/shaderClass.h"
+#include "../includes/Texture.h"
+
+void ProcessInput(GLFWwindow *window)
 {
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		// glfwSetWindowShouldClose(window, true);
+	}
 }
 
 int main()
 {
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	const unsigned int width = 800;
+	const unsigned int height = 800;
+	if (!glfwInit())
+	{
+		std::cout << "Failed to initialize GLFW\n";
+		return -1;
+	}
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    // Construct the window
-    GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Template", nullptr, nullptr);
-    if (!window)
-    {
-        std::cout << "Failed to create the GLFW window\n";
-        glfwTerminate();
-    }
+	// Construct the window
+	GLFWwindow *window = glfwCreateWindow(width, height, "OpenGL Template", nullptr, nullptr);
+	if (!window)
+	{
+		std::cout << "Failed to create the GLFW window\n";
+		glfwTerminate();
+		return -1; // Exit the program if the window creation fails
+	}
 
-    glfwMakeContextCurrent(window);
+	glfwMakeContextCurrent(window);
 
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize GLAD\n";
-        return -1;
-    }
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD\n";
+		glfwDestroyWindow(window);
+		glfwTerminate();
+		return -1;
+	}
 
-    // Handle view port dimensions
-    glViewport(0, 0, 800, 600);
-    glfwSetFramebufferSizeCallback(window, [](GLFWwindow* window, int width, int height)
-    {
-        glViewport(0, 0, width, height);
-    });
+	// Handle view port dimensions
+	glViewport(0, 0, width, height);
+	glfwSetFramebufferSizeCallback(window, [](GLFWwindow *window, int width, int height)
+								   { glViewport(0, 0, width, height); });
 
-    // This is the render loop
-    while (!glfwWindowShouldClose(window))
-    {
-        ProcessInput(window);
+	GLfloat vertices[] =
+		{
+			-0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+			-0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f,
+			0.5f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f,
+			0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f};
 
-        // Druids are the best
-        glClearColor(1.00f, 0.49f, 0.04f, 1.00f);
-        glClear(GL_COLOR_BUFFER_BIT);
+	GLuint indices[] =
+		{
+			0, 2, 1,
+			0, 3, 2};
 
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
+	Shader shaderProgram("../res/Shaders/default.vert", "../res/Shaders/default.frag");
+	VAO VAO1;
+	VAO1.Bind();
 
-    glfwTerminate();
-    return 0;
+	VBO VBO1(vertices, sizeof(vertices));
+	EBO EBO1(indices, sizeof(indices));
+
+	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 8 * sizeof(float), (void *)0);
+	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 8 * sizeof(float), (void *)(3 * sizeof(float)));
+	VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void *)(6 * sizeof(float)));
+	VAO1.Unbind();
+	VBO1.Unbind();
+	EBO1.Unbind();
+
+	GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
+
+	Texture bean("../res/Textures/bean.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGB, GL_UNSIGNED_BYTE);
+	bean.texUnit(shaderProgram, "tex0", 0);
+
+	// This is the render loop
+	while (!glfwWindowShouldClose(window))
+	{
+		ProcessInput(window);
+
+		// Druids are the best
+		glClearColor(0.00f, 0.49f, 0.04f, 1.00f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		shaderProgram.Activate();
+
+		glm::mat4 model = glm::mat4(1.0f);
+		glm::mat4 view = glm::mat4(1.0f);
+		glm::mat4 proj = glm::mat4(1.0f);
+		view = glm::translate(view, glm::vec3(0.0f, -0.5f, -2.0f));
+		proj = glm::perspective(glm::radians(45.0f), (float)(width / height), 0.1f, 100.0f);
+
+		int modelLoc = glGetUniformLocation(shaderProgram.ID, "model");
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		int viewLoc = glGetUniformLocation(shaderProgram.ID, "view");
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+		int projLoc = glGetUniformLocation(shaderProgram.ID, "proj");
+		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(proj));
+
+		glUniform1f(uniID, 0.5f);
+		bean.Bind();
+		VAO1.Bind();
+
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	VAO1.Delete();
+	VBO1.Delete();
+	EBO1.Delete();
+	bean.Delete();
+	shaderProgram.Delete();
+
+	glfwDestroyWindow(window);
+
+	glfwTerminate();
+	return 0;
 }
