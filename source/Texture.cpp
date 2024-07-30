540457cae3a5bc3e731c56746fbc38dc51e92a36
a1824273 <a1824273@student.adelaide.edu.au>
2024-07-30 15:32:00 +0930

    got different lighting types

--- source/Texture.cpp
+++ source/Texture.cpp
@@ -1,6 +1,6 @@
 #include "../includes/Texture.h"
 
-Texture::Texture(const char *image, GLenum texType, GLenum slot, GLenum format, GLenum pixelType)
+Texture::Texture(const char *image, GLenum texType, GLuint slot, GLenum format, GLenum pixelType)
 {
     type = texType;
     int widthImg, heightImg, numColCh;
@@ -8,7 +8,8 @@ Texture::Texture(const char *image, GLenum texType, GLenum slot, GLenum format, 
 
 
 	glGenTextures(1, &ID);
-	glActiveTexture(slot);
+	glActiveTexture(GL_TEXTURE0 + slot);
+	unit = slot;
 	glBindTexture(texType, ID);
 
 	glTexParameteri(texType, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
@@ -34,6 +35,7 @@ void Texture::texUnit(Shader& shader, const char *uniform, GLuint unit)
 }
 void Texture::Bind()
 {
+	glActiveTexture(GL_TEXTURE0 + unit);
     glBindTexture(type, ID);
 }
 void Texture::Unbind()
